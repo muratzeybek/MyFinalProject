74884eb3536e539ee643cccf6249584df3e4e6f7
MURAT ZEYBEK <zeybekmurat@msn.com>
2021-02-20 16:19:55 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // Çıplak Class Kalmasın... Ç.C.K Standardı
+    // Eğer ki bir class herhangi bir inheritance veya interface implementasyonu almıyorsa
+    // ileride problem yaşama ihtimalin yüksek. Bu varlıklarımızı işaretleme eğilimine 
+    // gruplama eğilimine gideriz. Deriz ki Concrete klasöründeki sınıflar, bir veritabanı 
+    // tablosuna karşılık geliyor. 
+    public class Category: IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
