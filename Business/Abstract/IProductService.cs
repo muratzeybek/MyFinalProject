881a997a2b2838bd0ec981a1958abb7cc1f4cbbe
projehesabim@hotmail.com <projehesabim@hotmail.com>
2021-02-22 16:02:20 +0300

    10.Ders Ara

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace Business.Abstract
         List<Product> GetAllByCategory(int id);
         List<Product> GetByUnitPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
+        IResult Add(Product product);
+        Product GetById(int produtId);
     }
 }
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; }
+        
+
+        public string Message { get; }
+    }
+}
